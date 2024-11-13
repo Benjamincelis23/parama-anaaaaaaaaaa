6a1b43e7a1bf671bbe163287f2e6170286d04d3e
Benjamincelis23 <benj.celis@duocuc.cl>
2024-11-13 10:15:41 -0300

    Reserva

---
+++ Videos/app dual/appErmi-main/appErmi-main/src/app/pages/reserva/reserva.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reserva', // Asegúrate de que este selector coincida con el nombre de la página
+  templateUrl: './reserva.page.html', // Asegúrate de que la plantilla esté en la ruta correcta
+  styleUrls: ['./reserva.page.scss'],
+})
+export class ReservaPage implements OnInit {  // Cambié "RecerbaPage" a "ReservaPage"
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
